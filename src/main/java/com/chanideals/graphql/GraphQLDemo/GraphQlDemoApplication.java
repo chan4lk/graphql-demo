a0a7c8122a4da4a8143e526eeb51bafb8735c2a5
Chandima Ranaweera <chan4lk@gmail.com>
2019-04-10 23:41:16 +0530

    add graphql

--- src/main/java/com/chanideals/graphql/GraphQLDemo/GraphQlDemoApplication.java
+++ src/main/java/com/chanideals/graphql/GraphQLDemo/GraphQlDemoApplication.java
@@ -2,12 +2,20 @@ package com.chanideals.graphql.GraphQLDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GraphQlDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphQlDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "Hello, GraphQL";
 	}
 
 }
